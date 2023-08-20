9e925854c0489bff452b26e398d62a07a8997628
DMZCON International Conference on Cyber Security <138613531+dmzcon@users.noreply.github.com>
2023-08-20 20:18:37 +0200

    Create consts.h

---
+++ RaspberryPi-and-STM32F4Discovery-friendship/RPi-device-software/consts.h
@@ -0,0 +1,29 @@
+////// DEFINE INPUTS AND OUTPUTS /////////
+
+
+#define INPUT_FIRST_INDEX				1
+
+#define	I_INPUT_1			1
+#define	I_INPUT_2			2
+
+
+#define	I_INPUT_27			27
+#define	I_INPUT_28			28
+
+#define INPUT_LAST_INDEX				28
+
+////////////////////////////////////////////////////
+
+#define OUTPUT_FIRST_INDEX			29
+
+#define	O_OUTPUT_1			29
+#define	O_OUTPUT_1			29
+#define	O_OUTPUT_1			29
+
+
+#define	O_OUTPUT_N			53    //   calculate:))
+
+#define OUTPUT_LAST_INDEX				53
+
+///////////////////// THE END //////////////////////
+
