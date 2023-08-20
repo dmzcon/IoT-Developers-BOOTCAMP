fd0846795a23d45de3a04c35275c092217dcd593
DMZCON International Conference on Cyber Security <138613531+dmzcon@users.noreply.github.com>
2023-08-20 20:50:29 +0200

    Create temp.cpp

---
+++ RaspberryPi-and-STM32F4Discovery-friendship/RPi-device-software/temp.cpp
@@ -0,0 +1,23 @@
+#include "temp.h"
+
+void digitalWrite(int p1,int p2) {
+
+}
+
+unsigned int digitalRead(int p) {
+
+return 0;
+}
+
+void pinMode(int p1,int p2) {
+
+}
+
+void pullUpDnControl(int p1,int p2) {
+
+}
+
+int wiringPiSetup() {
+
+return 0;
+}
